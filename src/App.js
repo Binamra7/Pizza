9ab791a1baab650ca60c8aff3e7135ab41728494
Binamra7 <binamra7khadka6@gmail.com>
2022-06-26 12:33:31 +0545

    fix: font changed to inter

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
-import Menu from "./pages/menu";
+import Menu from "./pages/Menu";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 
--- src/pages/menu.jsx
+++ src/pages/menu.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
-import { MenuList } from '../helpers/MenuList'
-import MenuItem from '../components/menuItem'
-import '../styles/menu.css'
+import React from "react";
+import { MenuList } from "../helpers/MenuList";
+import MenuItem from "../components/MenuItem";
+import "../styles/menu.css";
 
 function menu() {
-
-    return (
-        <div className="menu">
-            <h1 className="menuTitle">Our menu</h1>
-            <div className="menuList">
-                {MenuList.map((menuItem, key) => {
-                    return <MenuItem key={key} image={menuItem.image} name={ menuItem.name} price={menuItem.price}/>
-                })}
-            </div>           
-        </div>
-    )
+	return (
+		<div className="menu">
+			<h1 className="menuTitle">Our menu</h1>
+			<div className="menuList">
+				{MenuList.map((menuItem, key) => {
+					return (
+						<MenuItem
+							key={key}
+							image={menuItem.image}
+							name={menuItem.name}
+							price={menuItem.price}
+						/>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
 
-export default menu
+export default menu;
